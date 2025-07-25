58886601f25c7386168b1229f545ef97c2a05c49
r-shankaran <riyashankaran18@gmail.com>
2025-07-25 10:31:39 -0700

    Add UI to add friend's birthday

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
         Friend(name: "Riya", birthday: .now),
         Friend(name: "Bob", birthday: .now)
     ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack {
@@ -24,6 +26,24 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(Text("Birthdays"))
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection:$newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text:$newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = Date.now
+                    }.bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
         
     }
