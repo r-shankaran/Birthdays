f971081f89ff2ddebba2f3bea8bc6f33d7c66cc5
r-shankaran <riyashankaran18@gmail.com>
2025-07-25 09:58:36 -0700

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Riya Shankaran on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
